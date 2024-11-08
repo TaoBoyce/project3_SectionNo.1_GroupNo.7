ea36e41a9748e7d2aa7e7a08109324c3c4a815c0
Kaiden <kprovidence@conestogac.on.ca>
2024-11-08 14:21:09 -0500

    made classes for Lightscolor

--- Sec1Group7_Project/LightsColor.h
+++ Sec1Group7_Project/LightsColor.h
@@ -1,1 +1,30 @@
 #pragma once
+
+using namespace std;
+
+class color {
+	int value;
+	int brigthness;
+};
+
+class Red: public color {
+	char red;
+	int redBrightness;
+};
+
+class Green: public color {
+	char green;
+	int greenBrightness;
+};
+
+class Blue: public color {
+	char blue;
+	int blueBrightness;
+	int SetBlue() {
+
+	}
+};
+
+int main() {
+
+}
