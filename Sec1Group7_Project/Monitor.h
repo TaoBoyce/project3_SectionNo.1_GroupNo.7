575591d64fd3822b0563f79088f23607db54cabe
Tao Boyce (bitty45) <tao.z.boyce@gmail.com>
2024-11-08 13:11:10 -0500

    Add monitor header

---
+++ Sec1Group7_Project/Monitor.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <vector>
+#include <string>
+
+template <typename T>
+class Monitor {
+private:
+	std::vector<T> data;
+	T parseData(std::string line);
+protected:
+	T getFlux();
+	std::vector<T> readFromFile(std::string filename);
+};
+
+template <>
+int Monitor<int>::parseData(std::string line);
+template <>
+float Monitor<float>::parseData(std::string line);
+
