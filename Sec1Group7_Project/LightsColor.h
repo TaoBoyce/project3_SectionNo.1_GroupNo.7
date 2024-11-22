9aa48af01d89d1c56195855c1ca4dc2912a3d6f2
Kaiden <kprovidence@conestogac.on.ca>
2024-11-21 22:21:30 -0500

    completed lightscolor

--- Sec1Group7_Project/LightsColor.h
+++ Sec1Group7_Project/LightsColor.h
@@ -1,30 +1,80 @@
 #pragma once
+#include <iostream>
 
 using namespace std;
 
-class color {
-	int value;
-	int brigthness;
+class Color {
+protected:
+    int value;
+    int brightness;
+
+public:
+    Color() : value(0), brightness(50) {}
+
+    virtual ~Color() {}
+
+
+    int getValue() const { return value; }
+    void setvalue(int val) { brightness = val; }
+    int getBrightness() const { return brightness; }
+    void setBrightness(int bright) { brightness = bright; }
+
+
+    virtual void display() const = 0;
+
 };
 
-class Red: public color {
-	char red;
-	int redBrightness;
+class Red : public Color {
+public:
+    Red() : Color() {}
+    ~Red() {}
+
+    //display color
+    void display() const override {
+        std::cout << "Red\n" << value << "Brightness: " << brightness << "%" << std::endl;
+    }
 };
 
-class Green: public color {
-	char green;
-	int greenBrightness;
+class Green : public Color {
+public:
+    Green() : Color() {}
+    ~Green() {}
+
+    //display color
+    void display() const override {
+        std::cout << "Green\n" << value << "Brightness: " << brightness << "%" << std::endl;
+    }
+
 };
 
-class Blue: public color {
-	char blue;
-	int blueBrightness;
-	int SetBlue() {
+class Blue : public Color {
+public:
+    Blue() : Color() {}
+    ~Blue() {}
 
-	}
+    //display color
+    void display() const override {
+        std::cout << "Blue\n" << value << "Brightness: " << brightness << "%" << std::endl;
+    }
+};
+//this is the class that utilizes a color at a time
+class lightsColor {
+private:
+    Red red;
+    Blue blue;
+    Green green;
+public:
+    lightsColor() {}
+    ~lightsColor() {}
+
+    Red& getRed() { return red; }
+    Green& getGreen() { return green; }
+    Blue& getBlue() { return blue; }
+
+    void display() const {
+        red.display();
+        green.display();
+        blue.display();
+    }
 };
 
-int main() {
-
-}
