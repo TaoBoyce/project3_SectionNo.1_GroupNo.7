3d8619c512c520880d69434a501c415329b2f611
Tao Boyce (bitty45) <tao.z.boyce@gmail.com>
2024-11-28 20:25:36 -0500

    Revert "Reapply "inline functions""
    This reverts commit b16a32c3daedf2c2226563cc261968a9d7ed3112.

--- Sec1Group7_Project/FanSpeedMonitor.h
+++ Sec1Group7_Project/FanSpeedMonitor.h
@@ -1,14 +1,11 @@
 #pragma once
 #include "Monitor.h"
-#define MAX_FAN_RPM 2000
-#define FAN_FILE "FanSpeedData.txt"
 
-class FanSpeedMonitor:Monitor<int> {
+class FanSpeedMonitor :Monitor<int> {
 private:
 	int speedRPM;
 public:
 	FanSpeedMonitor();
-	FanSpeedMonitor(std::string filename);
 	int getRPM();
 	bool setRPM(int rpm);
 	int getFluctuatingRPM();
